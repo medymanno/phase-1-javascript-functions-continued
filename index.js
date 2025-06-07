4844638a8f683ec64a68007168642cfe397c646c
Medy <mannomedy@gmail.com>
2025-06-07 23:45:47 +0300

    add changes

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(joy= "roller-skate"){
+    return `This Saturday, I want to ${joy}!`;
+}
+saturdayFun();
+
+
+const mondayWork = function (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair ="*"){
+    return function(adjective = "spacial"){
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
